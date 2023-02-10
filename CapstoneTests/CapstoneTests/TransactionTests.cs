b165d5183ad497e2a00283fa29d94e78ef252b7b
John Lindsey <johnlindseyjl14@gmail.com>
2023-02-10 15:34:39 -0500

    Finished unit tests for Animal, Transaction, and VendingMachine classes

--- CapstoneTests/CapstoneTests/TransactionTests.cs
+++ CapstoneTests/CapstoneTests/TransactionTests.cs
@@ -9,11 +9,18 @@ namespace CapstoneTests
     [TestClass]
     public class TransactionTests
     {
+        Transaction transactiontransaction;
+
+        [TestInitialize] 
+        public void Init() 
+        {
+            transactiontransaction = new Transaction();
+        }
+
         [TestMethod]
         public void FeedMoneyTest()
         {
             // Arrange
-            Transaction transactiontransaction = new Transaction();
             decimal input = 2.00M;
 
             // Act
@@ -21,6 +28,42 @@ namespace CapstoneTests
 
             // Assert
             Assert.AreEqual(input, transactiontransaction.Balance);
+        }
+
+        [TestMethod]
+        public void DispenseTest()
+        {
+            // Arrange
+            string name = "Tori the Turtle";
+            string type = "Pony";
+            decimal price = 5.25M;
+            string slotID = "E1";
+            transactiontransaction.Balance = 6.00M;
+
+            // Act
+            Animal tori = new Animal(name, type, price, slotID);
+            transactiontransaction.Dispense(tori);
+
+            // Assert
+            Assert.AreEqual(0.75M, transactiontransaction.Balance);
+            Assert.AreEqual(4, tori.NumRemaining);
+        }
+
+        [DataTestMethod]
+        [DataRow(1.00, "1 dollar")]
+        [DataRow(1.15, "1 dollar, 1 dime, 1 nickel")]
+        [DataRow(29.55, "29 dollars, 2 quarters, 1 nickel")]
+        public void GiveChangeOutputTest(double balance, string expectedResult)
+        {
+            decimal decimalBalance = (decimal)balance;
+
+            transactiontransaction.Balance = decimalBalance;
+            string changeMessage = transactiontransaction.GiveChange();
+            expectedResult = $"Your change is {decimalBalance:C2}. You'll recieve: {expectedResult}. Thank you!";
+
+            Assert.AreEqual(0, transactiontransaction.Balance);
+            
+            Assert.AreEqual(expectedResult, changeMessage);
         }
     }
 }
---
+++ CapstoneTests/CapstoneTests/VendingMachineTests.cs
@@ -0,0 +1,40 @@
+﻿using Capstone;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class VendingMachineTests
+    {
+        VendingMachine vm;
+
+        [TestInitialize]
+        public void Init()
+        {
+            vm = new VendingMachine();
+        }
+
+        [DataTestMethod]
+        [DataRow("Cube Duck", "Duck", 2.50, "A2")]
+        [DataRow("Tabby Cat", "Cat", 2.50, "C3")]
+        [DataRow("Horse", "Pony", 0.90, "D3")]
+        public void ConstructorTest(string name, string type, double price, string slotID)
+        {
+            // Arrange
+            decimal decimalPrice = (decimal)price;
+            Animal animal = new Animal(name, type, decimalPrice, slotID);
+
+            // Act
+            Animal dictionaryValue = vm.SlotToAnimalDictionary[animal.SlotID];
+
+            // Assert
+            Assert.AreEqual(dictionaryValue.Name, animal.Name);
+            Assert.AreEqual(dictionaryValue.Price, animal.Price);
+            Assert.AreEqual(dictionaryValue.Type, animal.Type);
+            Assert.AreEqual(dictionaryValue.SlotID, animal.SlotID);
+        }
+    }
+}
